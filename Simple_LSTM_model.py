18ad136d15b7f7e86bf59c4183f4d5f60bee1475
sunyaofeng8 <sunyaofeng8@yeah.net>
2020-01-14 16:31:39 +0800

    fix some bug

--- Simple_LSTM_model.py
+++ Simple_LSTM_model.py
@@ -4,6 +4,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
+from sklearn.utils import shuffle
+
 def Build_Dict(trainset, testset):
     count_of_words = {}
     words = (' '.join(list(trainset['CleanedText'].values) + list(testset['CleanedText'].values))).split()
@@ -17,14 +19,14 @@ def Build_Dict(trainset, testset):
     dic = {}
     for word in count_of_words:
         if count_of_words[word] <= 10: # Nan Threshold
-            dic[word] = 0
-        dic[word] = len(dic) + 1
+            continue
+        dic[word] = len(dic)
     
     return dic
 
 def CleanedTextToTensor(raw_X):
     X = [x.split() for x in raw_X]
-    X = [[dic[t] for t in x] for x in X]
+    X = [[dic[t] for t in x if t in dic] for x in X]
     
     max_len = max([len(x) for x in X])
     X = [x + [0] * (max_len - len(x)) for x in X]
@@ -40,35 +42,52 @@ def ScoreToTensor(raw_Y):
     
     return Y
 
-if __name__ == "__main__":
-    trainset = pd.read_csv("./data/local_train_set.csv")
-    testset = pd.read_csv("./data/local_test_set.csv")
+trainset = pd.read_csv("./data/local_train_set.csv")
+testset = pd.read_csv("./data/local_test_set.csv")
 
-    dic = Build_Dict(trainset, testset)
-    Vocab_Size = len(dic) + 2 # Nan
+dic = Build_Dict(trainset, testset)
+Vocab_Size = len(dic) # Nan
 
-    print("Total vocabularies : %d" % Vocab_Size)
+print("Total vocabularies : %d" % Vocab_Size)
 
-    train_X = CleanedTextToTensor(trainset['CleanedText'].values)
-    print('train_X shape:', train_X.shape)
-    train_Y = ScoreToTensor(trainset['Score'].values)
-    print('train_Y shape:', train_Y.shape)
+train_X = CleanedTextToTensor(trainset['CleanedText'].values)
+print('train_X shape:', train_X.shape)
+train_Y = ScoreToTensor(trainset['Score'].values)
+print('train_Y shape:', train_Y.shape)
 
-    test_X = CleanedTextToTensor(testset['CleanedText'])
-    print('train_X shape:', test_X.shape)
-    test_Y = ScoreToTensor(testset['Score'])
-    print('train_Y shape:', test_Y.shape)
+test_X = CleanedTextToTensor(testset['CleanedText'])
+print('train_X shape:', test_X.shape)
+test_Y = ScoreToTensor(testset['Score'])
+print('train_Y shape:', test_Y.shape)
 
-    hidden_size = 128
-    model = keras.Sequential([
-        keras.layers.Embedding(Vocab_Size, hidden_size),
-        keras.layers.Bidirectional(keras.layers.LSTM(hidden_size)),
-        keras.layers.Dense(32, activation='relu'),
-        keras.layers.Dense(5, activation='sigmoid')
-    ])
+hidden_size = 128
 
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    print(model.summary())
+model = keras.Sequential([
+    keras.layers.Embedding(Vocab_Size, hidden_size),
+    keras.layers.Bidirectional(keras.layers.LSTM(hidden_size)),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(32, activation='relu'),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(5, activation='softmax')
+])
 
-    history = model.fit(x=train_X, y=train_Y, epochs = 10, validation_data=(test_X, test_Y), shuffle=
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+
+history = model.fit(x=train_X, y=train_Y, epochs = 3, validation_data=(test_X, test_Y), shuffle=
                     'steps_per_epoch')
+
+preds = model.predict(test_X)
+preds = preds.argmax(1)
+
+truths = testset['Score'].values - 1
+
+res = [[0]*5 for i in range(5)]
+
+for pred, truth in zip(preds, truths):
+    res[truth][pred] += 1
+
+tot = truths.size // 5
+
+for i in range(5):
+    print([x / tot for x in res[i]])
