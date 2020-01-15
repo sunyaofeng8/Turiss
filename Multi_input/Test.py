57519df5d2741803c1060f1b81ec1f33f890ab6c
Jiaqi Xi <45897890+jiaqi-xi@users.noreply.github.com>
2020-01-15 12:40:29 +0800

    Add files via upload
    Update Multi_input_3.py (change #input from 5 to 3, delete helpfulness-related inputs)
    newdic.pkl = dic generated by Test_3.py

---
+++ Multi_input/Test.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+import tensorflow as tf
+from tensorflow import keras
+
+import sys
+sys.path.insert(0, r'/Users/racoon727/Desktop/大学/Google/ML_winter_camp/Turiss-master/')
+
+from Multi_input import MultiInput
+
+trainset = pd.read_csv("../data/train_helpfulness.csv")
+testset = pd.read_csv("../data/test_helpfulness.csv")
+
+multi_input = MultiInput(trainset, testset, save=True, load=False, name1='newdic_product', name2='newdic_user', \
+                   name3='newdic_time', name4='newdic_help_nume', name5='newdic_help_deno')
+model = multi_input.getModel()
+
+print(model.summary())
+
