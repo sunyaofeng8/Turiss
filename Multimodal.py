f9b4381ae8450f8596d7aca9eaf0cd46afe10194
sunyaofeng8 <sunyaofeng8@yeah.net>
2020-01-15 12:16:26 +0800

    11

--- Multimodal.py
+++ Multimodal.py
@@ -10,12 +10,10 @@ from tensorflow.keras.models import Model
 import tensorflow_hub as hub
 
 train_dataset_fp = './data/local_train_set.csv'
-#test_dataset_fp = './data/local_test_set.csv'
+test_dataset_fp = './data/local_test_set.csv'
 
 trainset = pd.read_csv(train_dataset_fp)
-#testset = pd.read_csv(test_dataset_fp)
-
-trainset = trainset[:500]
+testset = pd.read_csv(test_dataset_fp)
 
 from utility import BertTokenizer, CleanedTextDict
 
@@ -24,15 +22,21 @@ bert_layer = hub.KerasLayer("./bert_layer", trainable=True)
 tokenizer = BertTokenizer(max_len = 256, bert_layer = bert_layer)
 #textDic = CleanedTextDict(trainset, testset)
 
-trainset['stokens'] = trainset['CleanedText'].apply(lambda x: tokenizer.GetStokens(x))
-trainset['input_ids'] = trainset['stokens'].apply(lambda x: tokenizer.GetInput_ids(x))
-trainset['input_masks'] = trainset['stokens'].apply(lambda x: tokenizer.GetInput_masks(x))
-trainset['input_segments'] = trainset['stokens'].apply(lambda x: tokenizer.GetInput_segments(x))
+def DatasetToTensor(trainset):
+    trainset['stokens'] = trainset['CleanedText'].apply(lambda x: tokenizer.GetStokens(x))
+    trainset['input_ids'] = trainset['stokens'].apply(lambda x: tokenizer.GetInput_ids(x))
+    trainset['input_masks'] = trainset['stokens'].apply(lambda x: tokenizer.GetInput_masks(x))
+    trainset['input_segments'] = trainset['stokens'].apply(lambda x: tokenizer.GetInput_segments(x))
 
-X = [tf.convert_to_tensor(trainset['input_ids'], dtype=tf.int32),
-    tf.convert_to_tensor(trainset['input_masks'], dtype=tf.int32),
-    tf.convert_to_tensor(trainset['input_segments'], dtype=tf.int32),
-]
+    X = [tf.convert_to_tensor(trainset['input_ids'], dtype=tf.int32),
+        tf.convert_to_tensor(trainset['input_masks'], dtype=tf.int32),
+        tf.convert_to_tensor(trainset['input_segments'], dtype=tf.int32),
+    ]
+
+    return X
+
+train_X = DatasetToTensor(trainset)
+test_X = DatasetToTensor(testset)
 
 def ScoreToTensor(raw_Y):
     Y = np.array(raw_Y) - 1 # convert to [0, 4]
@@ -41,7 +45,8 @@ def ScoreToTensor(raw_Y):
     
     return Y
 
-Y = ScoreToTensor(trainset['Score'])
+train_Y = ScoreToTensor(trainset['Score'])
+test_Y = ScoreToTensor(testset['Score'])
 
 max_len = 256
 
@@ -67,13 +72,13 @@ tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram
 history = model.fit(x=X, y=Y, epochs = 1, validation_split = 0.2, shuffle='steps_per_epoch', callbacks=[tensorboard_callback])
 '''
 
-history = model.fit(x=X, y=Y, epochs = 1, validation_split = 0.2, shuffle='steps_per_epoch')
+history = model.fit(x=train_X, y=train_Y, epochs = 1, validation_data = (test_X, test_Y), shuffle='steps_per_epoch')
 
 
-preds = model.predict(X)
+preds = model.predict(test_X)
 preds = preds.argmax(1)
 
-truths = trainset['Score'].values - 1
+truths = testset['Score'].values - 1
 
 res = [[0]*5 for i in range(5)]
 
